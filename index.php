0672d3e463bb6d6771b88f1d5d7cc21e82c6a12c
Diana Arismendi <dianajara82@gmail.com>
2020-12-01 23:37:07 -0500

    se realiza conversor y pruebas

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabajo Final Programación en PHP</title>
+    <!--Estilo para desktop-->
+    <link href="css/estilos.css" rel="stylesheet" type="text/css"/>
+    
+<body>
+    <header>
+        <img src="img/blue.jpg" id="header">
+    </header>
+    <h1>
+        TRABAJO FINAL PROGRAMACION EN PHP
+    </h1>
+    
+    <nav>
+        <ul id="menu">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="php/conversiones.php">Conversiones</a></li>
+            <li><a href="#">Matemáticas &#65516;</a>
+                <ul class="hidden">
+                    <li><a href="php/serie.php">Serie Fibonacci</a></li>
+                    <li><a href="php/factoriales.php">Obtener Factorial</a></li>
+                    <li><a href="php/comparacion.php">Mayor y menor</a></li>
+                </ul>
+            </li>
+            <li><a href="php/sobre.php">Sobre mí</a></li>
+            
+        </ul>
+    </nav>
+    <section>
+        <div id="caja1"></div>
+        <div id="caja2"></div>
+        <div id="caja3"></div>
+        <div id="caja4"></div>
+    </section>
+    <footer>
+        <img id="footer" src="img/footer.png">        
+    </footer>
+    
+</body>
+</html>
